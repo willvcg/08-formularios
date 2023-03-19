7ae767927a229824d62e2b480757d48f5c7e2376
Wilman Vinicio Cumbicus Guaman <wcumbicu@emeal.nttdata.com>
2023-03-19 11:35:41 +0100

    Crear componentes para formularios reactivos y templates, tambien sincroniizar las rutas correspondientes para los formularios

--- formularios/src/app/app.module.ts
+++ formularios/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateComponent } from './pages/template/template.component';
+import { ReactiveComponent } from './pages/reactive/reactive.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateComponent,
+    ReactiveComponent
   ],
   imports: [
     BrowserModule,
